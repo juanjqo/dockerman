64acd52d09fe23719a55deec114d65a1358309b1
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2025-07-31 19:28:52 +0100

    Added a script to run on macos. However, it is not working

---
+++ run_container_macos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Container name:" $1
+echo "Image:" $2
+# Argument validation check
+if [ $# -eq 0 ]; then
+    echo "Error: No arguments provided."
+    echo "Example: sh run_container.sh desired_container_name  docker_image"
+    exit 1
+fi
+
+if [ $# != 2 ]; then
+    echo "Error: You need to provide two arguments."
+    echo "Example: sh run_container.sh desired_container_name  docker_image"
+    exit 1
+fi
+
+xhost +local:root
+docker run -it \
+--rm --env=DISPLAY=docker.for.mac.host.internal:0 --volume /tmp/.X11-unix:/tmp/.X11-unix --privileged --network=host --ipc=host  \
+--name=$1 $2 /bin/bash
